20561813b75cfec274841c016bca58af3224aa0c
Unai <ullorent@c3r8s5.42urduliz.com>
2021-08-02 13:16:58 +0200

    Pequeño cambio a la función ft_strlcat.c

--- libft/ft_strlcat.c
+++ libft/ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: ullorent <ullorent@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/02 12:34:53 by ullorent          #+#    #+#             */
-/*   Updated: 2021/08/02 12:42:25 by ullorent         ###   ########.fr       */
+/*   Updated: 2021/08/02 13:15:07 by ullorent         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,22 +14,21 @@
 
 size_t	ft_strlcat(char *dst, const char *src, size_t n)
 {
+	size_t	c;
 	size_t	d;
-	size_t	e;
 
+	c = 0;
 	d = 0;
-	e = 0;
-	while (src[d] != '\0')
-		d++;
-	if (n != 0)
+	while (dst[c] != '\0')
 	{
-		while (src[e] != '\0' && (n - 1))
-		{
-			dst[e] = src[e];
-			e++;
-			n--;
-		}
-		dst[e] = '\0';
+		c++;
 	}
-	return (d);
+	while (src[d] != '\0' && (n - 1))
+	{
+		dst[c] = src[d];
+		c++;
+		d++;
+	}
+	dst[c] = '\0';
+	return (c + d);
 }
