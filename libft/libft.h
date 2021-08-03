66d4e528046dc2acb1c36daa4bb5282995806872
Unai <ullorent@c3r8s5.42urduliz.com>
2021-08-03 12:55:27 +0200

    Añadidas tres nuevas funciones / Cambiado el Makefile y la librería

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: ullorent <ullorent@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/02 11:54:48 by ullorent          #+#    #+#             */
-/*   Updated: 2021/08/02 12:42:41 by ullorent         ###   ########.fr       */
+/*   Updated: 2021/08/03 12:28:23 by ullorent         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,9 @@ int				ft_isalnum(int c);
 int				ft_isascii(int c);
 int				ft_isprint(int c);
 int				ft_strlen(char *str);
+int				ft_toupper(int c);
+int				ft_tolower(int c);
+char			*ft_strchr(const char *str, int c);
 size_t			ft_strlcpy(char *dst, const char *src, size_t n);
 size_t			ft_strlcat(char *dst, const char *src, size_t n);
 
