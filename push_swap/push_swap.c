d9887faec466972e2e80f1ee5c31bd2632b76804
AllPlayed <unaillope@gmail.com>
2022-01-19 17:30:34 +0100

    Proyecto push_swap comenzado y modificado

--- push_swap/push_swap.c
+++ push_swap/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: ullorent <ullorent@student.42urduliz.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/19 16:23:02 by ullorent          #+#    #+#             */
-/*   Updated: 2022/01/19 16:26:52 by ullorent         ###   ########.fr       */
+/*   Updated: 2022/01/19 16:43:30 by ullorent         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,18 +20,18 @@ int	main(int argc, char **argv)
 	int	c;
 
 	c = 0;
-	if (argc < 1)
+	if (argc < 2)
 	{
 		write(1, "Erorr\n", 6);
-		return (0);
+		exit (1);
 	}
 	while (argv[c])
 	{
-		if (!ft_isdigit((int)argv[c]))
+		if ((c <= 'z' && c >= 'a') || (c <= 'Z' && c >= 'A'))
 		{
 			write(1, "Error\n", 6);
 			write(1, "is not int\n", 11);
-			return (0);
+			exit (1);
 		}
 		c++;
 	}
