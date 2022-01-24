78b6c0c72367fa1bf2dbd0eff92e4c5163f36112
AllPlayed <unaillope@gmail.com>
2022-01-24 13:41:43 +0100

    Añadido checker de errores básico para el push_swap

--- push_swap/push_swap.c
+++ push_swap/push_swap.c
@@ -6,35 +6,45 @@
 /*   By: ullorent <ullorent@student.42urduliz.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/19 16:23:02 by ullorent          #+#    #+#             */
-/*   Updated: 2022/01/19 16:43:30 by ullorent         ###   ########.fr       */
+/*   Updated: 2022/01/24 13:40:16 by ullorent         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft/libft.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
+#include "push_swap.h"
+
+void	ft_numcheck(char **argv)
+{
+	int	c;
+	int	i;
+
+	c = 0;
+	i = 1;
+	while (argv[i] != NULL)
+	{
+		c = 0;
+		while (argv[i][c])
+		{
+			if ((int)((argv[i][c] >= ' ' && argv[i][c] <= '/') ||
+			(argv[i][c] >= ':' && argv[i][c] <= '~')))
+			{
+				write(1, "Error\n", 6);
+				write(1, "is not int\n", 11);
+				exit (1);
+			}
+			c++;
+		}
+		i++;
+	}
+}
 
 int	main(int argc, char **argv)
 {
-	int	c;
-
-	c = 0;
 	if (argc < 2)
 	{
-		write(1, "Erorr\n", 6);
+		write(1, "Error\n", 6);
 		exit (1);
 	}
-	while (argv[c])
-	{
-		if ((c <= 'z' && c >= 'a') || (c <= 'Z' && c >= 'A'))
-		{
-			write(1, "Error\n", 6);
-			write(1, "is not int\n", 11);
-			exit (1);
-		}
-		c++;
-	}
+	ft_numcheck((char **)argv);
 	write(1, "OK\n", 3);
 	return (0);
 }
---
+++ push_swap/push_swap.h
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ullorent <ullorent@student.42urduliz.co    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/24 13:20:59 by ullorent          #+#    #+#             */
+/*   Updated: 2022/01/24 13:41:22 by ullorent         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+# include "libft/libft.h"
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+
+/* typedef struct psmain_data {
+	int	c;
+	int	i;
+}				t_mdata; */
+
+/* -- main functions -- */
+void	ft_numcheck(char **argv);
+
+/* -- errors checker -- */
+
+/* -- commands functions -- */
+
+#endif
